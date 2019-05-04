84670bf3a7da855d9a549b6913e14321237e76cc
Morfey216 <Morfey4web@gmail.com>
2019-05-04 15:44:24 +0300

    render apartments from props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Main from '../main/main.jsx';
 
-const App = () => {
-  return <Main />;
+const App = (props) => {
+  const {titles} = props;
+  return <Main
+    apartments = {titles}
+  />;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
-const init = () => {
+const apartmentList = [
+  `Beautiful & luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`
+];
+
+const init = (apartments) => {
   ReactDOM.render(
-      <App />,
+      <App
+        titles = {apartments}
+      />,
       document.querySelector(`#root`)
   );
 };
 
-init();
+init(apartmentList);
