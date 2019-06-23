16afd4dddd889300961dd89a7ac2bcfad56cf53f
Morfey216 <Morfey4web@gmail.com>
2019-06-24 00:35:31 +0300

    created Favorites page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import {Switch, Route} from 'react-router-dom';
 import Main from "../main/main";
 import SignIn from "../sign-in/sign-in";
+import Favorites from "../favorites/favorites";
 
 const App = () => {
   return <Switch>
     <Route path="/" exact component={Main} />
     <Route path="/login" component={SignIn} />
+    <Route path="/favorites" component={Favorites} />
   </Switch>;
 };
 
