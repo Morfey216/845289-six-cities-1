48cd5a0b633cd6bf3fa02c005c311c9e59c66d1e
Morfey216 <Morfey4web@gmail.com>
2019-06-24 01:12:29 +0300

    created withAuthorization HOC

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,12 +3,14 @@ import {Switch, Route} from 'react-router-dom';
 import Main from "../main/main";
 import SignIn from "../sign-in/sign-in";
 import Favorites from "../favorites/favorites";
+import withAuthorization from "../../hocs/with-authorization/with-authorization";
+
 
 const App = () => {
   return <Switch>
     <Route path="/" exact component={Main} />
     <Route path="/login" component={SignIn} />
-    <Route path="/favorites" component={Favorites} />
+    <Route path="/favorites" component={withAuthorization(Favorites)} />
   </Switch>;
 };
 
