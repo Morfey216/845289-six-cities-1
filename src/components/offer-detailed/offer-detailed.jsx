1ef99cf0cc33d5f6f9aa012d9a2cadae7fa3475c
Morfey216 <Morfey4web@gmail.com>
2019-07-02 11:22:46 +0300

    corrected logo link

--- src/components/offer-detailed/offer-detailed.jsx
+++ src/components/offer-detailed/offer-detailed.jsx
@@ -84,7 +84,7 @@ const OfferDetailed = (props) => {
                 <h2 className="property__host-title">Meet the host</h2>
                 <div className="property__host-user user">
                   <div className={`property__avatar-wrapper user__avatar-wrapper ${host.isPro ? `property__avatar-wrapper--pro` : ``}`}>
-                    <img className="property__avatar user__avatar" src={`/${host.avatarUrl}`} width="74" height="74" alt="Host avatar"/>
+                    <img className="property__avatar user__avatar" src={`${host.avatarUrl}`} width="74" height="74" alt="Host avatar"/>
                   </div>
                   <span className="property__user-name">
                     {host.name}
